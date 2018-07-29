ed026656ba8bbddba4041035606eaa7590ba557f
yashg4509 <league@WTS-iMac-11.attlocal.net>
2018-07-29 12:55:56 -0700

    IP: Hangman

--- DataS&Algorithms-Level3-Gupta/src/_01_04_HangMan/HangMan.java
+++ DataS&Algorithms-Level3-Gupta/src/_01_04_HangMan/HangMan.java
@@ -13,7 +13,7 @@ import javax.swing.JPanel;
 public class HangMan{
 	Utilities util = new Utilities();
 	Stack<String> words = new Stack<>();
-	String content;
+	String[] content;
 	String word; 
 	JLabel label = new JLabel();
 	JFrame frame = new JFrame();
@@ -40,13 +40,13 @@ public class HangMan{
 	
 	void gameSetup() {
 		word = words.pop();
-		content = "_ ";
+		content = new String[word.length()];
 		
-		for (int i = 0; i < word.length(); i++) {
-			content += "_ ";
+		for (int i = 0; i < (word.length() - 1); i++) {
+			content[i] = "_ ";
 		}
 		
-		label.setText(content);
+		label.setText(contentToString());
 		pnl.add(label);
 		frame.add(pnl);
 		frame.pack();
@@ -59,14 +59,47 @@ public class HangMan{
 	void game() {
 		lives = 10;
 		
-		ArrayList<String> incorrectGuess = new ArrayList<>();
 		for (int i = 0; i < lives; i++) {
 			String guess = JOptionPane.showInputDialog("Enter a guess: ");
-			incorrectGuess.add(guess);
-
+			guessCheck(guess);
+			
+			if(lives < 10) {
+				JOptionPane.showMessageDialog(null, "Eh.....BADDDD!!!");
+			}
 		}
 		
+//			
+//			for (int j = 0; j < guess.length(); j++) {
+//				if(word.contains(guess.charAt(j) + "")) {
+//					content[word.indexOf(guess.charAt(j) + "")] = guess.charAt(j) + "";
+//					label.setText(contentToString());
+//					frame.pack();
+//				}
+//			}
+//			
+//		}
+		
+	} 
+	
+	public String contentToString() {
+		String str = "";
+		for (int i = 0; i < (content.length - 1); i++) {
+			str += content[i];
+		}
+		return str;
 	}
+	
+	public void guessCheck(String guess) {
+		for (int i = 0; i < word.length(); i++) {
+			char currentLetter = word.charAt(i);
+			if(currentLetter == guess.charAt(0)) {
+				content[i] = word.charAt(i) + "";
+				label.setText(contentToString());
+				frame.pack();
+			}
+		}
+	}
+	
 	public static void main(String[] args) {
 		HangMan player = new HangMan();
 		player.start();
